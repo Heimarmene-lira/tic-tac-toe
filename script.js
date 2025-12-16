be175fb267181890342751c1ea4491b8f568dd65
Heimarmene-lira <69810627+Heimarmene-lira@users.noreply.github.com>
2025-12-16 22:54:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const board = document.getElementById("board");
 const message = document.getElementById("message");
 const restartBtn = document.getElementById("restart");
 
-const BOT_TOKEN = "8504925989:AAF-isr5TpYcnfZk8ivLYY8p9ditrMMztFY";
-const CHAT_ID = "1234088555";
+const BOT_TOKEN = 8504925989:AAF-isr5TpYcnfZk8ivLYY8p9ditrMMztFY;
+const CHAT_ID = 1234088555;
 
 let cells = [];
 let gameActive = true;
